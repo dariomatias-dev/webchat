a05a551d1cbf9797109db4e8a2dd0bc689b73793
Dales10 <matiasdario75@gmail.com>
2023-03-31 08:04:59 -0300

    Firebase configured.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,29 @@
-const Home = () => {
-  return (
-    <>
+import { auth, GoogleAuthProvider, signInWithPopup } from '@/services/firebase';
 
-    </>
+const Home = () => {
+  const handleGoogleSignIn = () => {
+
+  };
+
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <div className='w-[600px] h-[400px] flex justify-center bg-zinc-900'>
+        <h1 className='text-4xl font-bold'>
+          Login
+        </h1>
+
+        <p>
+          Escolha um método de login:
+        </p>
+
+        <button
+          type='button'
+          onClick={handleGoogleSignIn}
+        >
+          Google
+        </button>
+      </div>
+    </div>
   );
 };
 
--- services/firebase.ts
+++ services/firebase.ts
@@ -1,18 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOFy2rsh51yFefmy6DSOhQn-jG184mfRo",
-  authDomain: "chat-db-756ef.firebaseapp.com",
-  databaseURL: "https://chat-db-756ef-default-rtdb.firebaseio.com",
-  projectId: "chat-db-756ef",
-  storageBucket: "chat-db-756ef.appspot.com",
-  messagingSenderId: "3109736746",
-  appId: "1:3109736746:web:536dafc27df9846bc1ce53",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { db };
+export { auth, db, GoogleAuthProvider, signInWithPopup };
