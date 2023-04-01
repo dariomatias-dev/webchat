a2403a03d64f4b349e7cccb3cb60c3a35543adbe
Dales10 <matiasdario75@gmail.com>
2023-04-01 14:19:00 -0300

    Login screen created.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import SignIn from '@/components/SignIn';
+import Login from '@/components/Login';
 
 const Home = () => {
   return (
     <div className='flex justify-center'>
-      <SignIn />
+      <Login />
     </div>
   );
 };
--- services/firebase.ts
+++ services/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from 'firebase/auth';
+import { getAuth, signInWithPopup, onAuthStateChanged, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore, collection, onSnapshot } from 'firebase/firestore';
 
 const firebaseConfig = {
