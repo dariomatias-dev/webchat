44997e7e17bf5b7ce63b8bf04ac8949a9fa01448
Dales10 <matiasdario75@gmail.com>
2023-04-05 09:43:14 -0300

    Restyled messages and added automatic scrolling to the bottom of the page.

--- components/Chat/index.tsx
+++ components/Chat/index.tsx
@@ -1,13 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { AiOutlineSend } from 'react-icons/ai';
 
 import { db, collection, addDoc, query, orderBy, limit, onSnapshot, getDocs } from "@/services/firebase";
 import { useData } from "../Context";
+import Image from "next/image";
 
 type MessagesProps = {
     userUid: string;
     message: string;
-    send: Date;
+    send: {
+        seconds: number;
+        milliseconds: number;
+    };
 };
 
 const Chat = () => {
@@ -15,7 +19,9 @@ const Chat = () => {
     const [messages, setMessages] = useState<MessagesProps[]>([]);
     const [enableMessagesUpdate, setEnableMessagesUpdate] = useState(false);
 
-    const { userUid } = useData();
+    const { userUid, users } = useData();
+
+    const refChat = useRef<HTMLDivElement>(null);
 
     const sendMessage = () => {
         addDoc(collection(db, 'messages'), {
@@ -64,6 +70,8 @@ const Chat = () => {
     };
 
     useEffect(() => {
+        const height = refChat.current?.scrollHeight;
+        refChat.current?.scrollTo(0, height || 0);
         if (messages.length && enableMessagesUpdate)
             updateMessages();
     }, [messages, enableMessagesUpdate]);
@@ -75,16 +83,37 @@ const Chat = () => {
     return (
         <>
             <div className="w-[600px] h-screen">
-                <div className="w-full h-full flex flex-col gap-4 p-10 bg-black overflow-auto pb-24">
+                <div ref={refChat} className="w-full h-full flex flex-col gap-6 pt-10 px-3 pb-24 bg-black overflow-auto scroll-smooth">
                     {
-                        messages.map((message: any, index) => {
+                        messages.map((message: MessagesProps, index) => {
                             return (
-                                <p
+                                <div
                                     key={index}
-                                    className="flex max-w-max bg-[#00b5e0] py-2 px-4 rounded-xl"
+                                    style={userUid === message.userUid ? { flexDirection: 'row-reverse' } : {}}
+                                    className="flex gap-2"
                                 >
-                                    {message.message}
-                                </p>
+                                    <Image
+                                        src={users[userUid].photoUrl}
+                                        width={1}
+                                        height={1}
+                                        alt="User photo."
+                                        className="w-10 h-10 rounded-full"
+                                    />
+                                    <div
+                                        style={userUid === message.userUid ? { backgroundColor: '#00b5e0', borderTopLeftRadius: '16px', borderTopRightRadius: '0' } : {}}
+                                        className="flex flex-col gap-2 max-w-[75%] bg-zinc-900 py-2 px-4 rounded-tr-2xl rounded-br-2xl rounded-bl-2xl"
+                                    >
+                                        <span
+                                            style={userUid === message.userUid ? { color: '#FFFFFF' } : {}}
+                                            className='text-zinc-300'
+                                        >
+                                            {users[message.userUid]?.name || 'Dário Matias'}
+                                        </span>
+                                        <p>
+                                            {message.message}
+                                        </p>
+                                    </div>
+                                </div>
                             )
                         })
                     }
